756dae8e12fb0a1fb3d4e623eb828dc2141265c9
Exivac <exivacz@gmail.com>
2022-04-05 10:37:38 -0300

    CR Finish 05HW

--- 05-EstructuraDeDatos-III/homework/homework.js
+++ 05-EstructuraDeDatos-III/homework/homework.js
@@ -14,44 +14,72 @@ function BinarySearchTree(val) {
   this.value = val
   this.left = null
   this.right = null
-  
+
   this.gSize = 1
 }
 
 BinarySearchTree.prototype.insert = function(val){
   let node = new BinarySearchTree(val)
   
-  if (val <= this.value && this.left == null){
+  if (val <= this.value && this.left == null){ //Create a new node in left side if val is less-than value
     this.left = node
     this.gSize++
-    return node.value
-  } else if (val > this.value && this.right == null){
+    return
+
+  } else if (val > this.value && this.right == null){ //Create new node in Right side if val is more-than value
     this.right = node
     this.gSize++
-    return node.value
-  } else if (val <= this.value && this.left !== null){
+    return
+
+  //vv If neither of the above, rerun the insertion with the corresponding side vv//
+
+  } else if (val <= this.value && this.left !== null){ //Less-than case
     return this.left.insert(val)
-  } else if (val > this.value && this.right !== null){
+
+  } else if (val > this.value && this.right !== null){ //More-than case
     return this.right.insert(val)
   }
+  //^^ If neither of the above, rerun the insertion with the corresponding side ^^//
 }
 
-
 BinarySearchTree.prototype.contains = function(val){
-  if (this.value === val) return true
+  if (this.value == val) return true
   
-  if (val <= this.value && !this.left){
+  if (val <= this.value && this.left !== null){
     return this.left.contains(val)
-  } else if (val > this.value && !this.right){
+  } else if (val > this.value && this.right !== null){
     return this.right.contains(val)
   }
   return false
 }
 
-BinarySearchTree.prototype.depthFirstForEach = function(){
+BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
+
+  
+  if (order === 'pre-order'){
+    cb(this.value)
+    if(this.left)this.left.depthFirstForEach(cb, order)
+    if(this.right)this.right.depthFirstForEach(cb, order)
+  }
+  
+  if (order === 'post-order'){
+    if(this.left)this.left.depthFirstForEach(cb, order)
+    if(this.right)this.right.depthFirstForEach(cb, order)
+    cb(this.value)
+  }
+  
+  if (!order || order === 'in-order'){
+    if(this.left)this.left.depthFirstForEach(cb, order)
+    cb(this.value)
+    if(this.right)this.right.depthFirstForEach(cb, order)
+  }
 }
 
-BinarySearchTree.prototype.breadthFirstForEach = function(){
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, arr = []){
+  cb (this.value)
+  if (this.left) arr.push(this.left)
+  if(this.right) arr.push(this.right)
+  if (arr.length) arr.shift().breadthFirstForEach(cb, arr)
 }
 
 BinarySearchTree.prototype.size = function(){
